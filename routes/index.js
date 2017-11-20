2784a363f34b445b64bd8cca52cda8ecd7373e2b
Aaron Lee <leeaaron326@gmail.com>
2017-11-20 01:34:58 -0500

    testing shit

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,25 @@ router.get('/', function(req, res, next) {
 router.post('/upload-file', function(req, res, next) {
 	if (req.files) {
 		var file = req.files.inputFileName;
-		var filename = file.name;
-		file.mv("../uploads/" + filename, function(err) {
-			if (err) {
-				// Error
-				console.log("Error");
-				res.send("App error: " + err);
-			} else {
-				// Do nothing
-				console.log("Success");
-				res.send("Success");
-			}
-		});
+		var filename = file.name
+
+		var loc = window.location.pathname;
+		var dir = loc.substring(0, loc.lastIndexOf('/'));
+
+		res.send('<p>Dir = ' + dir + '</p>\n'
+			+ '<p>Loc = ' + loc + '</p>');
+
+		// file.mv("../uploads/" + filename, function(err) {
+		// 	if (err) {
+		// 		// Error
+		// 		console.log("Error");
+		// 		res.send("App error: " + err);
+		// 	} else {
+		// 		// Do nothing
+		// 		console.log("Success");
+		// 		res.send("Success");
+		// 	}
+		// });
 	} else {
 		res.send("<h1>Nothing happnened</h1>");
 	}
